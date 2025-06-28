9f2aeaf09e101e69b7b129cb93ef5fd99661b902
aytony <anyang@buaa.edu.cn>
2025-06-28 09:02:18 +0800

    feat: 完成任务 2：LRU 缓存策略

--- src/storage/lru_buffer_strategy.cpp
+++ src/storage/lru_buffer_strategy.cpp
@@ -5,13 +5,23 @@ namespace huadb {
 void LRUBufferStrategy::Access(size_t frame_no) {
   // 缓存页面访问
   // LAB 1 BEGIN
-  return;
+
+  if (auto it{frame_to_list_.find(frame_no)}; it != frame_to_list_.end()) {
+    frame_order_.erase(it->second);
+  }
+  frame_order_.push_back(frame_no);
+  frame_to_list_[frame_no] = std::prev(frame_order_.end());
 };
 
 size_t LRUBufferStrategy::Evict() {
   // 缓存页面淘汰，返回淘汰的页面在 buffer pool 中的下标
   // LAB 1 BEGIN
-  return 0;
+
+  size_t frame_no{frame_order_.front()};
+  frame_order_.pop_front();
+  frame_to_list_.erase(frame_to_list_.find(frame_no));
+
+  return frame_no;
 }
 
 }  // namespace huadb
--- src/storage/lru_buffer_strategy.h
+++ src/storage/lru_buffer_strategy.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <list>
+#include <unordered_map>
+
 #include "storage/buffer_strategy.h"
 
 namespace huadb {
@@ -8,6 +11,10 @@ class LRUBufferStrategy : public BufferStrategy {
  public:
   void Access(size_t frame_no) override;
   size_t Evict() override;
+
+ private:
+  std::list<size_t> frame_order_;                                          // 缓存页面链表，按照最近访问顺序排序
+  std::unordered_map<size_t, std::list<size_t>::iterator> frame_to_list_;  // 页面编号到链表的映射
 };
 
 }  // namespace huadb
